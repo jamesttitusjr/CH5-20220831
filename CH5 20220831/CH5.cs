ba50df409c32f39b4c5c00cd9430de760b6b9186
James Titus <jamesttitusjr@outlook.com>
2022-08-31 11:57:02 -0400

    Add project files.

---
+++ CH5 20220831/CH5.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH5_20220831
+{
+    internal class CH5
+    {
+    }
+}
